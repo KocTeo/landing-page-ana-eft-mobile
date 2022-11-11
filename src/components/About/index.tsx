fb3575678d6249249ecf1c7da121061b61be6be1
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-11-11 17:34:34 -0300

    fix: About text

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -10,8 +10,8 @@ const About: React.FC = () => {
       <p>
         Olá, sou Ana Lucia Bettio Terapeuta Holística, formada em administração
         de empresas. Atuo nesta área de terapia holística a quase três anos e
-        hoje ajudo pessoas a olharem para seus traumas de maneira manneira que
-        nao traga mais sofrimento, a elevarem sua autoestima, eliminando crenças
+        hoje ajudo pessoas a olharem para seus traumas de maneira que não traga
+        mais sofrimento, a elevarem sua autoestima, eliminando crenças
         limitantes em todos os níveis da vida. Tenho como objetivo fazer com que
         o maior número de pessoas que chegarem até mim possa ter suas vidas
         transformadas.
